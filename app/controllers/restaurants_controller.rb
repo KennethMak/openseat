29d780f65c609b6b9a15761b23c0868eb41bd9c5
Kenneth <kennethmak@Kenneths-MacBook-Pro.local>
2014-09-07 20:49:57 -0400

    generated controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,51 @@
 class RestaurantsController < ApplicationController
+	before_filter :load_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
+  	@restaurants = Restaurant.all
   end
 
   def show
   end
 
   def new
+  	@restaurant = Restaurant.new
   end
 
   def edit
   end
+
+  def create
+  	@restaurant = Restaurant.new(restaurant_params)
+  	if @restaurant.save
+  		redirect_to restaurants_url
+  	else
+  		render :new
+  	end
+  end
+
+  def update
+  	if @restaurant.update_attributes(restaurant_params)
+  		redirect_to restaurants_url
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+  	@restaurant.destroy
+  		redirect_to restaurants_url
+  end
+
+
+  private
+
+  def restaurant_params
+  	params.require(:restaurant).permit(:name, :location, :description, :max_occupy)
+  end
+
+  def load_restaurant
+  	@restaurant = Restaurant.find(params[:id])
+  end
+
 end
