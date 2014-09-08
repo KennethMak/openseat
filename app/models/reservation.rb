4cfdbc615feba0993aa71417287f3be14d2c9383
Kenneth <kennethmak@Kenneths-MacBook-Pro.local>
2014-09-08 18:34:56 -0400

    added reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,13 @@
 class Reservation < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :restaurant
+	validate :availability
+
+	private
+
+	def availability
+		unless restaurant.available?(group_size, start)
+			errors.add(:base, "Sorry, this restaurant is full!")
+		end
+	end
 end
